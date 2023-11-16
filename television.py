5432669ae6e2cd5c395a8a6a97cea1e103c47551
katherineorozco <150992954+katherineorozco@users.noreply.github.com>
2023-11-16 14:08:55 -0600

    Fixed logic issues

--- television.py
+++ television.py
@@ -17,14 +17,15 @@ class Television:
             self.__status = True
 
     def mute(self):
-        if self.__muted:
-            self.__muted = False
-        else:
-            self.__muted = True
+        if self.__status:
+            if self.__muted:
+                self.__muted = False
+            else:
+                self.__muted = True
 
     def channel_up(self):
         if self.__status:
-            if self.__channel > Television.MAX_CHANNEL:
+            if self.__channel < Television.MAX_CHANNEL:
                 self.__channel += 1
             else:
                 self.__channel = Television.MIN_CHANNEL
@@ -40,15 +41,18 @@ class Television:
         if self.__status:
             self.__muted = False
             if self.__volume < Television.MAX_VOLUME:
-                self.__volume += 1
+                    self.__volume += 1
 
     def volume_down(self):
         if self.__status:
             self.__muted = False
             if self.__volume > Television.MIN_VOLUME:
-                self.__volume -= 1
+                    self.__volume -= 1
+
+
 
     def __str__(self):
-        return f"Power = [{self.__status}], Channel = [{self.__channel}], Volume = [{self.__volume}]"
-
-
+        if self.__muted == True:
+            return f"Power = [{self.__status}], Channel = [{self.__channel}], Volume = [{Television.MIN_VOLUME}]"
+        else:
+            return f"Power = [{self.__status}], Channel = [{self.__channel}], Volume = [{self.__volume}]"
