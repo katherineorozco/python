384d66ce02ce041862436a0043699e043a5121d4
katherineorozco <150992954+katherineorozco@users.noreply.github.com>
2023-11-16 15:02:49 -0600

    test init method

--- test_television.py
+++ test_television.py
@@ -2,8 +2,9 @@ import pytest
 from television import Television
 
 class Test:
-    def test_init():
+    def test_init(self):
         assert Television()._Television__status is False
         assert Television()._Television__muted is False
         assert Television()._Television__volume == Television.MIN_VOLUME
         assert Television()._Television__channel == Television.MIN_CHANNEL
+        assert str(Television()) == Television().__str__()
