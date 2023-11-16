226447d0c44acc8306390d66a39f110cff433519
katherineorozco <150992954+katherineorozco@users.noreply.github.com>
2023-11-16 14:35:47 -0600

    added docstrings and type hinting

--- television.py
+++ television.py
@@ -1,58 +1,83 @@
 class Television:
-    MIN_VOLUME = 0
-    MAX_VOLUME = 2
-    MIN_CHANNEL = 0
-    MAX_CHANNEL = 3
+    """
+    A class representing a television object.
+    """
+    MIN_VOLUME: int = 0
+    MAX_VOLUME: int = 2
+    MIN_CHANNEL: int = 0
+    MAX_CHANNEL: int = 3
 
-    def __init__(self):
-        self.__status = False
-        self.__muted = False
-        self.__volume = Television.MIN_VOLUME
-        self.__channel = Television.MIN_CHANNEL
+    def __init__(self) -> None:
+        """
+        Method to set Television object with default values.
+        """
+        self.__status: bool = False
+        self.__muted: bool = False
+        self.__volume: int = Television.MIN_VOLUME
+        self.__channel: int = Television.MIN_CHANNEL
 
-    def power(self):
+    def power(self) -> None:
+        """
+        Toggles the power status of the television.
+        """
         if self.__status:
             self.__status = False
         else:
             self.__status = True
 
-    def mute(self):
+    def mute(self) -> None:
+        """
+        Toggles the mute status of the television if it is powered on.
+        """
         if self.__status:
             if self.__muted:
                 self.__muted = False
             else:
                 self.__muted = True
 
-    def channel_up(self):
+    def channel_up(self) -> None:
+        """
+        Increases the channel number by 1 if the television is powered on.
+        """
         if self.__status:
             if self.__channel < Television.MAX_CHANNEL:
                 self.__channel += 1
             else:
                 self.__channel = Television.MIN_CHANNEL
 
-    def channel_down(self):
+    def channel_down(self) -> None:
+        """
+        Decreases the channel number by 1 if the television is powered on.
+        """
         if self.__status:
             if self.__channel > Television.MIN_CHANNEL:
                 self.__channel -= 1
             else:
                 self.__channel = Television.MAX_CHANNEL
 
-    def volume_up(self):
+    def volume_up(self) -> None:
+        """
+        Increases the volume level by 1 if the television is powered on.
+        """
         if self.__status:
             self.__muted = False
             if self.__volume < Television.MAX_VOLUME:
-                    self.__volume += 1
+                self.__volume += 1
 
-    def volume_down(self):
+    def volume_down(self) -> None:
+        """
+        Decreases the volume level by 1 if the television is powered on.
+        """
         if self.__status:
             self.__muted = False
             if self.__volume > Television.MIN_VOLUME:
-                    self.__volume -= 1
+                self.__volume -= 1
 
-
-
-    def __str__(self):
-        if self.__muted == True:
+    def __str__(self) -> str:
+        """
+        Returns a string representation of the television's status.
+        """
+        if self.__muted:
             return f"Power = [{self.__status}], Channel = [{self.__channel}], Volume = [{Television.MIN_VOLUME}]"
         else:
             return f"Power = [{self.__status}], Channel = [{self.__channel}], Volume = [{self.__volume}]"
